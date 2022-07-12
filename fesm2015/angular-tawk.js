891d52a9d6b916460b79b1bffc40db746ef97030
Max Leggieri <maxleggieri@gmail.com>
2022-07-12 12:29:04 +0200

    Removed /default from tawk.to links

--- fesm2015/angular-tawk.js
+++ fesm2015/angular-tawk.js
@@ -43,7 +43,7 @@ class AngularTawkComponent {
                 (function () {
                 var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
                 s1.async = true;
-                s1.src = 'https://embed.tawk.to/${this.id}/default';
+                s1.src = 'https://embed.tawk.to/${this.id}';
                 s1.charset = 'UTF-8';
                 s1.setAttribute('crossorigin', '*');
                 s0.parentNode.insertBefore(s1, s0);
